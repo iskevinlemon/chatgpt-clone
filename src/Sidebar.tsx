76342dfd8ac2340309622aaa7a1e481482ab0cc9
Kevin <126497052+iskevinlemon@users.noreply.github.com>
2024-01-20 20:53:40 +0800

    Updated new chat button

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -5,18 +5,17 @@ const Sidebar = () => {
         <>
             <div className="p-1 text-white">
 
-                <div className="gpt-new-chat-btn px-4 py-2 mt-2 mb-1 ml-2">
+                <div className="gpt-new-chat-btn px-2 py-2 mt-2 mb-1 ml-2">
+                    <i className="float-right mt-2 mr-1 far fa-pen-to-square"></i>
                     <span className="d-flex">
                         <span className="gpt-circle-2 text-dark">
                             <SmallSvg/>
                         </span>
-                        <div className="ml-2 mt-1 d-flex">
-                            New chat
-                            <div className="ml-5">
-                                <i className="far fa-pen-to-square"></i>
-                            </div>
+                        <div className="ml-2 mt-1">
+                            <small>New chat</small>
                         </div>
                     </span>
+                    
                 </div>
 
                 <div className="px-4 mt-3">
